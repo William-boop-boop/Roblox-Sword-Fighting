74f3c751ea60a37eb91bd05116fd6e4f4a9f4c61
William-boop-boop <williamleyten@gmail.com>
2025-09-21 21:34:05 -0400

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,1 +1,12 @@
+local M = {}
 
+-- Decision loop frequency (Hz)
+M.HEARTBEAT_DECISION_HZ = 5
+
+-- Tag placed on bot Models you want controlled
+M.BOT_TAG = "AIBot"
+
+-- Feature size used by FeatureEncoder (only relevant if you later swap to a real policy)
+M.FEATURE_DIM = 4
+
+return M
